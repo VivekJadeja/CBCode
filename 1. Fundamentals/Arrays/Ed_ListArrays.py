99507b616142e07c4a14196de30a9e75a1fee40f
VivekJadeja <57203251+VivekJadeja@users.noreply.github.com>
2020-07-24 15:40:12 -0500

    Notes on Arrays and List TC

---
+++ 1. Fundamentals/Arrays/Ed_ListArrays.py
@@ -0,0 +1,3 @@
+# Time complexity of accessing an element of a list is O(1)
+# TC of inserting or deleting at head is O(n)
+# TC of inseting or deleting at tail is O(1) if using .append(i) and .pop(i) methods. O(n) if done as a data structure array
